3eb84e973d55bb0a73c7f74c52485b33903aebb2
steffyjk <steffy.jk2018@gmail.com>
2025-06-05 19:20:19 +0530

    [ feat ]: added Get ME api [ get info regarding current logged In user

--- app/api/routes/user.py
+++ app/api/routes/user.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import Session
 from app.api.schemas.user import UserCreate, UserResponse
 from app.services.user import create_user
 from app.db.dependency import get_db
+from app.models.user import User
+from app.core.deps import get_current_user
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
@@ -10,3 +12,8 @@ router = APIRouter(prefix="/users", tags=["Users"])
 def register_user(user: UserCreate, db: Session = Depends(get_db)):
     # You can add checks for duplicates here if needed
     return create_user(db, user)
+
+
+@router.get("/me", response_model=UserResponse)
+def read_current_user(current_user: User = Depends(get_current_user)):
+    return current_user
